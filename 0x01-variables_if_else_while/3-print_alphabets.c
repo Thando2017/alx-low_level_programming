b54f4c55a83d856790296084644455b9c0d4bb17
Thando2017 <khuthi.r@gmail.com>
2023-08-11 14:09:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet.
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+char alp[52]="abcdefghijklmnopwrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZ"; int i;
+     for (i=0,i < 52; i++)
+{
+	putchar(alp[i]):
+}
+putchar(alp('\n'):
+return (0):
+}
