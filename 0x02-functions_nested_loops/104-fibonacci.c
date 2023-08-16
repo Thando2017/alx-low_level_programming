77f37300a55bbfdd0d00933f7c2f2a2775d96818
Thando2017 <khuthi.r@gmail.com>
2023-08-15 22:16:46 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 
-	/**
-	 * main - fibonacci <3
-	 *
-	 * Purpose - no hardcode
-	 *
-	 * Return:  (Success)
-	 */
+/**
+* main - describe fibonacci <3
+* Purpose - no hardcod
+*
+* Return:  (Success)
+*/
 
 
 	int main(void)
