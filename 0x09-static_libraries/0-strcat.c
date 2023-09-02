d5aebd2b539243fda79dd6e10b78944678e31434
Thando2017 <khuthi.r@gmail.com>
2023-09-02 16:33:16 -0700

    libmy.a, main.h

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -27,17 +27,3 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
-
-1 - isdigit.c
-
-#include "main.h"
-
-/**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
