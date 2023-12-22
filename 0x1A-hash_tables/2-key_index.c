bf79da604e107f2b116df2b19c603edc83d55148
Thando2017 <khuthi.r@gmail.com>
2023-12-22 13:28:53 -0800

    file

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,24 +1,13 @@
 #include "hash_tables.h"
 
-
 /**
-
- * key_index - get index for key given
-
- * @key: key (string) to hash
-
- * @size: size of hash table
-
+ * key_index - Get index for the given key.
+ * @key: Key (string) to hash.
+ * @size: Size of the hash table.
  *
-
- * Return: index of 'key' using djb2
-
+ * Return: Index of 'key' using djb2.
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-
 {
-
-        return (hash_djb2(key) % size);
-
+	return (hash_djb2(key) % size);
 }
